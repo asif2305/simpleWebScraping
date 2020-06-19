217508dbe600aed7dcdc682f560193681ab7cd17
Asif Ahmed <mygithub2305@gmail.com>
2020-06-19 12:27:05 +0200

    added file

---
+++ backend/commonUrl.js
@@ -0,0 +1,7 @@
+module.exports={
+    baseUrl:"https://www.amazon.com",
+    mainCatUrlInitial:"https://www.amazon.com/s/ref=nb_sb_noss?url=",
+    mainCatUrlLast:"&field-keywords=",
+    proxyList:"http://www.freeproxylists.net/?fbclid=IwAR0HRXOi3O8l1f1BObB7D5udiRjTKgmpNMeZF1DACRe9JxSODIBCMdai-0c"
+    
+}
